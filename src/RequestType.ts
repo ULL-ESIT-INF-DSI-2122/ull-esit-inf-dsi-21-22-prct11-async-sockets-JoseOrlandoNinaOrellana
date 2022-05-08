16f6733a1f9d32d39c6e2f03bc4fbd7048daf16f
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-05-08 12:17:51 +0000

    añadir tipo nuevo

--- src/RequestType.ts
+++ src/RequestType.ts
@@ -2,7 +2,7 @@
  * Petición del cliente
  */
 export type RequestType = {
-    type: 'add' | 'update' | 'remove' | 'read' | 'list';
+    type: 'add' | 'update' | 'remove' | 'read' | 'list' | 'none';
     user: string
     title?: string;
     body?: string;
