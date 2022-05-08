312940b1a0276022e28d73ede6f8705bf560d381
Jose Orlando Nina Orellana <alu0101322308@ull.edu.es>
2022-05-08 12:18:10 +0000

    añadir comentarios

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,21 @@ import { NoteManager } from './NoteManager';
 import { ResponseType } from './ResponseType';
 import { MessageEventEmitter } from './MessageEventEmitter';
 
+// Creamos el server
 net.createServer((connection) => {
+    // Creamos un objeto MessageEventEmitter
     const getRequest = new MessageEventEmitter(connection);
+
+    // Registramos un manejador para el evento message
     getRequest.on('message', (request) => {
+        // Creamos objetos y mostramos por pantalla la petición
         let manager: NoteManager = new NoteManager();
         let note: Note;
         let output: string = '';
         console.log('Request from ' + request.user + ':');
         console.log(request);
 
+        // Comprobamos el tipo de petición y la ejecutamos
         if(request.type === 'add') {
             note = new Note(request.title as string, request.body as string, request.color as string);
             output += manager.writeNote(request.user, note);
@@ -29,11 +35,22 @@ net.createServer((connection) => {
         else if(request.type === 'list') {
             output += manager.listNotes(request.user as string);
         }
+        else {
+            output = 'The command does not exists';
+        }
 
+        // Creamos un objeto respuesta
         let response: ResponseType = {
             response: output,
         }
-        connection.write(JSON.stringify(response) + '\n');
+
+        // Escribimos la respuesta
+        connection.write(JSON.stringify(response) + '\n', (err) => {
+            if(err)
+                console.error(err.message);
+        });
+
+        // Cerramos la conexión
         connection.end();
     });
 }).listen(60300, () => {
